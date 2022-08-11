631b09e58715feee9aba98789e11f1a772e9ba5a
Tatsiana Palikarpava <t.polikarpova@pixelplex.io>
2022-08-11 13:13:42 +0300

    [L1] Refactor

--- Lecture_1(Introduction)/ecmascript/src/index.js
+++ Lecture_1(Introduction)/ecmascript/src/index.js
@@ -1,11 +1,11 @@
 import readline from 'readline-sync';
 import * as cat from './cat.js';
-import { voice } from './cat.js';
+import { voice as catVoice } from './cat.js';
 import { Dog } from './dog.js';
 
 const n = readline.questionInt('Enter positive number\n');
 console.log(`Cat says ${cat.voice(n)}`);
-console.log(`Cat says ${voice(n)}`);
+console.log(`Cat says ${catVoice(n)}`);
 
 const myDog = new Dog('Rex');
 myDog.greeting();
